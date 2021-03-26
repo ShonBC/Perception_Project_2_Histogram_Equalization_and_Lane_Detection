f31f8a29860692858386bf5cafd31eb54be65d8f
ShonBC <scortes3@umd.edu>
2021-03-25 21:23:38 -0700

    240 fps

--- histogram_equalization.py
+++ histogram_equalization.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
 
     # Define Video out properties
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter('projection.mp4', fourcc, 20.0, (1920, 1080))
+    out = cv2.VideoWriter('projection.mp4', fourcc, 240, (1920, 1080))
         
     while True:
         ret, frame = cap.read()
