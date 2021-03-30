fe4246ec72c471f4cd3b8a63bb6a60463311a4d2
ShonBC <scortes3@umd.edu>
2021-03-30 13:54:52 -0700

    Cleaned main function added mp4 file to gitignore

--- histogram_equalization.py
+++ histogram_equalization.py
@@ -63,6 +63,42 @@ def gamma_corret(frame, gamma = 1): # Gamma Correction method
 
     return corr_frame
 
+def color(frame): # Histogram Equilization on color channels
+
+    """
+    Break frame out into the 3 color channels
+    Calculate PMF
+    Calculate CDF
+    Multiply each pixel with its corrisponding CDF value for each channel
+    Combine the color channels
+    Display the frame
+    """
+
+    b, g, r = cv2.split(frame) # Separate frame into 3 color channels
+
+    # Calculate probability mass function
+    b_PMF = pmf(b)
+    g_PMF = pmf(g)
+    r_PMF = pmf(r)
+
+    # Calculate Cumulative Distribution Function (CDF)
+    b_CDF = cdf(b_PMF)
+    g_CDF = cdf(g_PMF)
+    r_CDF = cdf(r_PMF)
+
+    # Apply equilization
+    b_eq = equalize(b,b_CDF)
+    g_eq = equalize(g, g_CDF)
+    r_eq = equalize(r, r_CDF)  
+
+        # Combine channels
+    final = cv2.merge((b_eq, g_eq, r_eq))
+
+    # Gamma Correction
+    corr_frame = gamma_corret(gray, gamma = 1)
+
+    cv2.imshow("Color Equilization", corr_frame) # Show the projection
+
 # Define Video out properties
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter('projection.mp4', fourcc, 240, (720,480))
@@ -78,56 +114,25 @@ if __name__ == "__main__":
         
         frame = cv2.resize(frame, (720,480))
 
-        """
-        Break frame out into the 3 color channels
-        Calculate PMF
-        Calculate CDF
-        Multiply each pixel with its corrisponding CDF value for each channel
-        Combine the color channels
-        Display the frame
+    
 
-        """
+        cv2.imshow("Video", frame)
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # Gamma Correction
-        corr_frame = gamma_corret(gray, gamma = .1)
-        cv2.imshow("Video Feed", corr_frame) # Show the projection
-
-
-        # b, g, r = cv2.split(frame) # Separate frame into 3 color channels
-
-        # # Calculate probability mass function
-        # b_PMF = pmf(b)
-        # g_PMF = pmf(g)
-        # r_PMF = pmf(r)
+        gamma_frame = gamma_corret(gray, gamma = .1)
+        cv2.imshow("Gamma Corrected", gamma_frame) # Show the projection
 
         gray_PMF = pmf(gray)
 
-        # # Calculate Cumulative Distribution Function (CDF)
-        # b_CDF = cdf(b_PMF)
-        # g_CDF = cdf(g_PMF)
-        # r_CDF = cdf(r_PMF)
-
         gray_CDF = cdf(gray_PMF)
-
-        # # Apply equilization
-        # b_eq = equalize(b,b_CDF)
-        # g_eq = equalize(g, g_CDF)
-        # r_eq = equalize(r, r_CDF)  
 
         gray_eq = equalize(gray, gray_CDF) 
 
-        # # Combine channels
-        # final = cv2.merge((b_eq, g_eq, r_eq))
+        cv2.imshow('Gray Equilization', gray_eq)
 
-        # # Gamma Correction
-        # corr_frame = gamma_corret(gray, gamma = 1)
-
-        # cv2.imshow("Video Feed", gray) # Show the projection
-
-        out.write(corr_frame)
+        out.write(gray_eq)
     
-
         # Condition to break the while loop
         i = cv2.waitKey(50)
         if i == 27:
