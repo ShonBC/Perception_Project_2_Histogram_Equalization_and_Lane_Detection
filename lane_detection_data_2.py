d0ae11be7a350696c6472033cd79145034ffb9dd
ShonBC <scortes3@umd.edu>
2021-04-01 20:41:56 -0700

    Added Homography method

--- lane_detection_data_2.py
+++ lane_detection_data_2.py
@@ -1,5 +1,6 @@
 import cv2
 import lane_detection_data_1
+import numpy as np
 
 #Camera Matrix
 K = [[  1.15422732e+03,   0.00000000e+00,   6.71627794e+02],
@@ -9,6 +10,20 @@ K = [[  1.15422732e+03,   0.00000000e+00,   6.71627794e+02],
 #Distortion Coefficients
 dist = [[ -2.42565104e-01,  -4.77893070e-02,  -1.31388084e-03,  -8.79107779e-05,
     2.20573263e-02]]
+
+def top_down(img): # Homography for top down view
+
+    w, h = 200, 500
+
+    src_points = np.float32([[320, 320], [440, 320], [160,460], [625, 460]])
+    dst_points = np.float32([[0, 0], [w, 0], [0, h], [w, h]])
+    
+    M, mask = cv2.findHomography(src_points, dst_points)
+    # M = cv2.getPerspectiveTransform(src_points, dst_points)
+
+    top = cv2.warpPerspective(img, M, (w, h))
+
+    return top
 
 
 if __name__ == "__main__":
@@ -27,11 +42,13 @@ if __name__ == "__main__":
             break
         
         frame = cv2.resize(frame, (720,480))
+        top = top_down(frame) # Use Homography for top down view
+        cv2.imshow('top', top)
         
         cv2.imshow("frame", frame)
         # out.write(frame)
         
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(100) & 0xFF == ord('q'):
             break
 
     cv2.waitKey(0)
