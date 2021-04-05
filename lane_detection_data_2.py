d5f92d89ea1568d3856a8d857af69f8605d2f3e4
ShonBC <scortes3@umd.edu>
2021-04-04 20:01:03 -0700

    Removed extra function and line in main. Updated video output frame size. Saved video output.

--- lane_detection_data_2.py
+++ lane_detection_data_2.py
@@ -183,64 +183,11 @@ def draw_poly(frame, draw):
 
     cv2.polylines(frame, [draw], False, (255, 0, 0), 3)
 
-def main_hough():
-
-     # # Define Video out properties
-    # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    # out = cv2.VideoWriter('lane_detection_2.mp4', fourcc, 240, (1920, 1080))
-
-    cap = cv2.VideoCapture('media/data_2/challenge_video.mp4')
-
-    ylines = []
-    wlines = []
-
-    while True:
-         
-        ret, frame = cap.read()
-
-        if not ret: # If no frame returned, break the loop
-            break
-        
-        frame = cv2.resize(frame, (720,480))
-        height, width, _ = frame.shape
-        frame = undistort(frame) # Undistort frame with camera matrix
-        top, _ = homog(frame)
-        _, front = homog(top)
-        cv2.imshow('back', front)
-
-        ymask, wmask = color_mask(top)
-        ycanny, ycontours = cnts(ymask)
-        wcanny, wcontours = cnts(wmask)
-        y_lines = h_lines(ycanny)
-        w_lines = h_lines(wcanny)
-        if y_lines is None:
-            y_lines = ylines
-        else:
-            ylines = y_lines
-        if w_lines is None:
-            w_lines = wlines
-        else:
-            wlines = w_lines
-        show_lines(ycanny,y_lines)
-        show_lines(wcanny, w_lines)
-        cv2.imshow('ycanny', ycanny)
-        cv2.imshow('wcanny', wcanny)
-        cv2.imshow('wmask', wmask)
-        cv2.imshow('ymask', ymask)
-
-
-        if cv2.waitKey(100) & 0xFF == ord('q'):
-            break
-
-    cv2.waitKey(0)
-    cap.release()
-    cv2.destroyAllWindows()
-
 if __name__ == "__main__":
 
-    # # Define Video out properties
-    # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    # out = cv2.VideoWriter('lane_detection_2.mp4', fourcc, 240, (1920, 1080))
+    # Define Video out properties
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter('lane_detection_2.mp4', fourcc, 240, (720,480))
 
     cap = cv2.VideoCapture('media/data_2/challenge_video.mp4')
 
@@ -280,8 +227,7 @@ if __name__ == "__main__":
 
         cv2.imshow('frame', frame)
         
-        # cv2.imshow("frame", frame)
-        # # out.write(frame)
+        out.write(frame)
         
         if cv2.waitKey(100) & 0xFF == ord('q'):
             break
